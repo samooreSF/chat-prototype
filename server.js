dcd228a233eb95ef9ff0ca8f6b3c68e4c778d771
Satchel Moore <samoore@davidson.edu>
2020-04-22 22:57:57 -0500

    help

--- server.js
+++ server.js
@@ -2,18 +2,10 @@ let express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    users = [];
-    var players = {};
-    var star = {
-        x: Math.floor(Math.random() * 700) + 50,
-        y: Math.floor(Math.random() * 500) + 50
-        };
-    var scores = {
-        blue: 0,
-        red: 0
-        };
+    users=[];
 
 //specify the html we will use
+
 app.use('/', express.static(__dirname + '/www'));
 //app.use('/',express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
@@ -21,15 +13,6 @@ app.get('/', function (req, res) {
   });
 
 
-//app.use('/', express.static(__dirname + 'index.html'));
-// app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
-// app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
-// app.get('/', (req, res) => {
-//     res.send(`
-//         <a href="streamer">Streamer</a><br>
-//         <a href="client">Client</a>
-//     `);
-// });
 
 //bind the server to the 80 port
 //server.listen(3000);//for local test
@@ -65,52 +48,5 @@ io.on('connection', function(socket) {
     //new image get
     socket.on('img', function(imgData, color) {
         socket.broadcast.emit('newImg', socket.nickname, imgData, color);
-    });
-
-    console.log('a user connected: ', socket.id);
-    // create a new player and add it to our players object
-    players[socket.id] = {
-      rotation: 0,
-      x: Math.floor(Math.random() * 700) + 50,
-      y: Math.floor(Math.random() * 500) + 50,
-      playerId: socket.id,
-      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-    };
-    // send the players object to the new player
-    socket.emit('currentPlayers', players);
-    // send the star object to the new player
-    socket.emit('starLocation', star);
-    // send the current scores
-    socket.emit('scoreUpdate', scores);
-    // update all other players of the new player
-    socket.broadcast.emit('newPlayer', players[socket.id]);
-
-    // when a player disconnects, remove them from our players object
-    socket.on('disconnect', function () {
-      console.log('user disconnected: ', socket.id);
-      delete players[socket.id];
-      // emit a message to all players to remove this player
-      io.emit('disconnect', socket.id);
-    });
-
-    // when a player moves, update the player data
-    socket.on('playerMovement', function (movementData) {
-      players[socket.id].x = movementData.x;
-      players[socket.id].y = movementData.y;
-      players[socket.id].rotation = movementData.rotation;
-      // emit a message to all players about the player that moved
-      socket.broadcast.emit('playerMoved', players[socket.id]);
-    });
-
-    socket.on('starCollected', function () {
-      if (players[socket.id].team === 'red') {
-        scores.red += 10;
-      } else {
-        scores.blue += 10;
-      }
-      star.x = Math.floor(Math.random() * 700) + 50;
-      star.y = Math.floor(Math.random() * 500) + 50;
-      io.emit('starLocation', star);
-      io.emit('scoreUpdate', scores);
     });
   });
