3d36161e33edad838468ec64477e60318fb7e279
Satchel Moore <samoore@davidson.edu>
2020-04-22 22:31:04 -0500

    style changes

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express = require('express'),
+let express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     users = [];
+    var players = {};
+    var star = {
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50
+        };
+    var scores = {
+        blue: 0,
+        red: 0
+        };
+
 //specify the html we will use
 app.use('/', express.static(__dirname + '/www'));
+//app.use('/',express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+
 //app.use('/', express.static(__dirname + 'index.html'));
 // app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
 // app.get('/streamer', (req, res) => res.sendFile(path.resolve(__dirname, './index.html')));
@@ -21,7 +37,7 @@ server.listen(process.env.PORT || 3000);//publish to heroku
 //server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
 //console.log('server started on port'+process.env.PORT || 3000);
 //handle the socket
-io.sockets.on('connection', function(socket) {
+io.on('connection', function(socket) {
     //new user login
     socket.on('login', function(nickname) {
         if (users.indexOf(nickname) > -1) {
@@ -50,4 +66,51 @@ io.sockets.on('connection', function(socket) {
     socket.on('img', function(imgData, color) {
         socket.broadcast.emit('newImg', socket.nickname, imgData, color);
     });
-});
+
+    console.log('a user connected: ', socket.id);
+    // create a new player and add it to our players object
+    players[socket.id] = {
+      rotation: 0,
+      x: Math.floor(Math.random() * 700) + 50,
+      y: Math.floor(Math.random() * 500) + 50,
+      playerId: socket.id,
+      team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+    // send the star object to the new player
+    socket.emit('starLocation', star);
+    // send the current scores
+    socket.emit('scoreUpdate', scores);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    // when a player disconnects, remove them from our players object
+    socket.on('disconnect', function () {
+      console.log('user disconnected: ', socket.id);
+      delete players[socket.id];
+      // emit a message to all players to remove this player
+      io.emit('disconnect', socket.id);
+    });
+
+    // when a player moves, update the player data
+    socket.on('playerMovement', function (movementData) {
+      players[socket.id].x = movementData.x;
+      players[socket.id].y = movementData.y;
+      players[socket.id].rotation = movementData.rotation;
+      // emit a message to all players about the player that moved
+      socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    socket.on('starCollected', function () {
+      if (players[socket.id].team === 'red') {
+        scores.red += 10;
+      } else {
+        scores.blue += 10;
+      }
+      star.x = Math.floor(Math.random() * 700) + 50;
+      star.y = Math.floor(Math.random() * 500) + 50;
+      io.emit('starLocation', star);
+      io.emit('scoreUpdate', scores);
+    });
+  });
